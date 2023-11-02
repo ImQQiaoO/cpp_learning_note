f026574c4fe24a85f7d1a82c5567b4a3d43c2628
ImQQiaoO <imqqiaoo@gmail.com>
2023-11-02 23:39:33 +0800

    feat: 理解“友元与继承”中“每个类负责控制自己的成员的访问权限”的概念

--- cpp/Quote/Disc_quote.h
+++ cpp/Quote/Disc_quote.h
@@ -9,6 +9,9 @@ public:
     Disc_quote(const std::string &book, double price, std::size_t qty, double disc) :
         Quote(book, price), quantity(qty), discount(disc) {}
     double net_price(std::size_t) const override = 0; // 纯虚函数
+    std::pair<size_t, double> discount_policy() const {
+        return { quantity, discount };
+    }
 protected:
     std::size_t quantity = 0;   // 折扣适用的购买量
     double discount = 0.0;      // 表示折扣的小数值
